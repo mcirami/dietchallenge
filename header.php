48755d1bf19456a1c6006a66cb8207a7fac63e37
Matteo Cirami <matteo@mscwebservices.net>
2020-03-20 12:06:59 -0500

    some styling adjustments and acf integration

--- header.php
+++ header.php
@@ -12,7 +12,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
-		<script src="https://www.paypal.com/sdk/js?client-id=sb&currency=USD" data-sdk-integration-source="button-factory"></script>
+		<!--<script src="https://www.paypal.com/sdk/js?client-id=sb&currency=USD" data-sdk-integration-source="button-factory"></script>-->
+		<script src="https://www.paypal.com/sdk/js?client-id=AY8hS778F2utYNwR5yVu1ozkxMWD9ooxBPFt7L_1zXiiYE7Ooof250bRNa4h6-Mun5whjrn3M4sdfSWw&currency=USD" data-sdk-integration-source="button-factory"></script>
 
 		<?php wp_head(); ?>
 		<script>
@@ -35,10 +36,14 @@
 			<header class="header clear row py-4 py-md-5" role="banner">
 				<div class="col-12">
 					<!-- logo -->
-					<div class="logo w-auto float-none float-md-left">
-						<a href="<?php echo home_url(); ?>">
+					<div class="logo d-flex align-content-center justify-content-center float-none float-md-left">
+						<a class="d-inline-block my-auto d-flex mr-1" href="<?php echo home_url(); ?>">
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="Logo" class="logo-img w-auto m-auto">
+							<h2 class="text-uppercase m-0 p-0">Buddy</h2>
+							<!--<img src="<?php /*echo get_template_directory_uri(); */?>/images/logo.jpg" alt="Logo" class="logo-img w-auto m-auto">-->
+						</a>
+						<a class="d-inline-block my-auto d-flex black" href="<?php echo home_url(); ?>">
+							<h3 class="m-0 p-0">system challenge</h3>
 						</a>
 					</div>
 					<!-- /logo -->
--- page-template-sales.php
+++ page-template-sales.php
@@ -18,8 +18,13 @@ $paymentType = (isset($_GET['paymentType']) ? $_GET['paymentType'] : "");
 
 		<section class="row hero">
 			<div class="col-12 col-md-6 mr-auto text-center">
-				<h3>"I feel beautiful and free as the water flowing down these rocks when I'm attempting to achieve my weight loss goals"</h3>
-				<h2 class="text-uppercase my-3 my-md-5">Hello Buddies!</h2>
+				<?php $headerQuote = get_field('header_quote');
+
+				if ($headerQuote) : ?>
+					<h3>"<?php echo $headerQuote; ?>"</h3>
+				<?php endif; ?>
+
+				<h2 class="text-uppercase my-3 my-md-5">Hello Buddy!</h2>
 				<img class="w-25 d-block mx-auto" src="<?php echo bloginfo( 'template_url' ); ?>/images/logo-30-days.png" alt="waterfall"/>
 				<h4 class="text-uppercase my-4">Buddy System</h4>
 				<h4 class="red text-uppercase mb-3">Weight Loss</h4>
@@ -30,25 +35,33 @@ $paymentType = (isset($_GET['paymentType']) ? $_GET['paymentType'] : "");
 		<section class="row intro_section">
 			<article class="col-12">
 				<div class="container">
-					<div class="row">
-						<div class="col-6 mx-auto py-5">
-							<div class="video_wrapper">
-								<?php /*echo do_shortcode("[advanced_iframe src='https://www.youtube.com/embed/i3CuqiJ0k4A']"); */?>
-								<iframe src="https://www.youtube.com/embed/i3CuqiJ0k4A" frameborder="0" allowfullscreen></iframe>
+
+					<?php $introSection = get_field('introduction_section');
+						if($introSection) :
+					?>
+
+							<div class="row">
+								<div class="col-10 col-md-8 col-lg-6 mx-auto py-5">
+									<!--<div class="video_wrapper">-->
+										<!--<iframe src="https://www.youtube.com/embed/i3CuqiJ0k4A" frameborder="0" allowfullscreen></iframe>-->
+									<?php echo $introSection['introduction_video']; ?>
+									<!--</div>-->
+								</div>
 							</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-12 text-center">
-							<p class="px-4">
-								Greetings Future Buddy's! Will you join me in a <span>30-day weight loss challenge?</span> The plan is super simple and easy to follow. You can begin your challenge at anytime. Many Buddy's have reported <span>losing up to 15 pounds</span>. Are you up for the challenge? I sure hope so!
-								You can begin this challenge once you <span>send a one time payment of $20.00</span> to CashApp or PayPal along with submitting your email. Upon receipt <span>I will perosnally email the information material</span> to begin the challenge. You will also receive an invite to the private Facebook group for Buddy Sytem Weight Loss Challenge.
-							</p>
-							<p>
-								Remember there are no diet pills, drinks, smoothies or mixtures. All you need is plain everyday food from the grocery store.
-							</p>
-						</div>
-					</div> <!-- text row -->
+							<div class="row">
+								<div class="col-12 text-center">
+
+									<p class="px-4">
+										<?php echo $introSection['introduction_text']; ?>
+									</p>
+									<p>
+										<?php echo $introSection['introduction_sub_text']; ?>
+									</p>
+
+								</div>
+							</div> <!-- text row -->
+						<?php endif; ?>
+
 					<div class="row images">
 						<img class="position-absolute top ribbon_img" src="<?php echo bloginfo( 'template_url' ); ?>/images/ribbon.png"/>
 						<div class="col-5 mx-auto">
@@ -89,13 +102,13 @@ $paymentType = (isset($_GET['paymentType']) ? $_GET['paymentType'] : "");
 		<section id="payment_form">
 			<div class="container">
 				<div class="row d-flex justify-content-center align-content-center flex-row">
-					<div class="col-12 col-md-6 d-flex flex-column px-4 pr-md-5 pl-md-0 mb-5 my-md-auto">
+					<div class="col-12 col-md-6 d-flex flex-column py-5 px-4 pr-md-5 pl-md-0 mb-2 my-md-auto">
 						<h2>Payment Methods</h2>
 						<h3>Once materials are emailed, no refund will be permitted.</h3>
 						<p>
 							<span>It is currently not possible to pay with CashApp online.</span> However, if you would like to pay with CashApp, Select CashApp at the top of the form on the right, submit your information then send $20 to Username: <span>$desi800</span>.
 						</p>
-						<p class="red">
+						<p class="yellow">
 							Disclaimer: Results may vary, consult your physician before beginning, no refunds after start up materials are emailed, no liablity.
 						</p>
 					</div>
@@ -127,11 +140,11 @@ $paymentType = (isset($_GET['paymentType']) ? $_GET['paymentType'] : "");
 										</div>
 										<div class="form-group mt-4">
 											<label class="mb-0" for="email">E-mail address</label><br>
-											<small id="emailHelp" class="form-text text-muted">You must include a valid email before clicking on payment button.</small>
-											<input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" placeholder="Enter Your Email Address">
+											<input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" placeholder="Enter Your Email Address" required>
 											<div id="emailError" class="my-4">
 												<p>Please enter a valid email address.</p>
 											</div>
+											<small id="emailHelp" class="form-text text-muted">You must include a valid email before clicking on payment button.</small><br>
 											<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 										</div>
 
@@ -172,28 +185,8 @@ $paymentType = (isset($_GET['paymentType']) ? $_GET['paymentType'] : "");
 												}
 											});
 
-											/*document.querySelector('#name').addEventListener('change', function(event) {
-												if(event.target.value !== "") {
-													submit = true;
-												}
-											});
-
-											if (submit === true) {
-												actions.enable();
-											} else {
-												inputError.style.display = 'block';
-											}*/
-
 										},
 
-										/*onClick: function() {
-											let email = document.valueOf('#email');
-											let name = document.valueOf('#name');
-
-											if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) || (name === "")) {
-												document.querySelector('#email_error').classList.remove('hidden');
-											}
-										},*/
 										style: {
 											shape: 'rect',
 											color: 'gold',
@@ -205,9 +198,12 @@ $paymentType = (isset($_GET['paymentType']) ? $_GET['paymentType'] : "");
 											return actions.order.create({
 												purchase_units: [{
 													amount: {
-														value: '30.00'
-													}
-												}]
+														value: '20.00',
+													},
+												}],
+												"application_context" : {
+													shipping_preference:"NO_SHIPPING"
+												}
 											});
 										},
 										onApprove: function(data, actions) {
@@ -231,14 +227,30 @@ $paymentType = (isset($_GET['paymentType']) ? $_GET['paymentType'] : "");
 
 							if ($paymentType == "cashapptype") :
 								?>
-								<h3>Thank you for submitting your information for the Buddy System Challenge!</h3>
-								<p>You selected to pay with CashApp. Please be sure to send your payment in CashApp to username.</p>
-								<h4>IMPORTANT: Do not forget to include the email you submitted in the form so I know who the payment is from.</h4>
-								<p>Once I receive the payment I will email you the Buddy System PDF and an invite to our private Facebook group!</p>
-								<p>Thank You!</p>
+								<div class="row confirmation">
+									<div class="col-12 m-auto">
+										<?php $cashAppConfirm = get_field('cashapp_confirmation');
+											if($cashAppConfirm) :
+										?>
+											<h3><?php echo $cashAppConfirm['line_one']?></h3>
+											<p><?php echo $cashAppConfirm['line_two']?></p>
+											<h4><?php echo $cashAppConfirm['line_three']?></h4>
+											<h5><?php echo $cashAppConfirm['line_four']?></h5></div>
+									<?php endif; ?>
+								</div>
 							<?php  else : ?>
-								<h3>Thank you for submitting your information for the Buddy System Challenge!</h3>
-								<p>You will get an email from me shortly with the PDF and an invite to our private Facebook group!</p>
+								<div class="row confirmation">
+									<div class="col-12 m-auto">
+										<?php $payPalConfirm = get_field('paypal_confirmation');
+											if($payPalConfirm) :
+										?>
+												<h3><?php echo $payPalConfirm['line_one']?></h3>
+												<h5><?php echo $payPalConfirm['line_two']?></h5>
+
+											<?php endif; ?>
+									</div>
+								</div>
+
 							<?php endif; ?>
 
 						<?php endif; ?>
@@ -247,7 +259,7 @@ $paymentType = (isset($_GET['paymentType']) ? $_GET['paymentType'] : "");
 			</div><!-- container -->
 		</section>
 
-		<section class="success_stories text-center">
+		<!--<section class="success_stories text-center">
 			<div class="container">
 				<div class="row">
 					<div class="col-12">
@@ -255,8 +267,8 @@ $paymentType = (isset($_GET['paymentType']) ? $_GET['paymentType'] : "");
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-5 mx-auto">
-						<img src="<?php echo bloginfo( 'template_url' ); ?>/images/video-placeholer-success.jpg"/>
+					<div class="col-10 col-md-8 col-lg-6 mx-auto">
+						<img src="<?php /*echo bloginfo( 'template_url' ); */?>/images/video-placeholer-success.jpg"/>
 					</div>
 				</div>
 				<div class="row">
@@ -265,45 +277,45 @@ $paymentType = (isset($_GET['paymentType']) ? $_GET['paymentType'] : "");
 					</div>
 				</div>
 			</div>
-		</section>
+		</section>-->
 
 		<section class="book_section">
 			<div class="container">
 				<div class="row d-flex justify-content-center align-content-center flex-row">
-					<div class="col-12 col-md-6 d-flex flex-column mb-5 my-md-auto py-5 py-md-0">
-						<h2 class="mb-4">Let's rev up and get excited about this and change the globe one buddy at a time!</h2>
-						<p class="mb-4">
-							For <span>only $20</span> potential buddies will receive the following materials for success during the challenge:
-						</p>
-						<ol>
-							<li>
-								<p>
-									A detailed food item list.
+
+					<?php $bookSection = get_field('book_section');
+						if ($bookSection) :
+					?>
+							<div class="col-12 col-md-6 d-flex flex-column mb-5 my-md-auto py-5 py-md-0">
+
+								<h2 class="mb-4">
+									<?php echo $bookSection['heading']; ?>
+								</h2>
+
+								<p class="mb-4">
+									<?php echo $bookSection['description']; ?>
 								</p>
-							</li>
-							<li>
-								<p>
-									A A 4 week Monday through Friday easy to perform exercise plan.
-								</p>
-							</li>
-							<li>
-								<p>
-									Step-by-Step Instructions
-								</p>
-							</li>
-							<li>
-								<p>
-									An invitation to the private Facebook support group.
-								</p>
-							</li>
-							<li>
-								<p>
-									A weekly inspirational buddy talk.
-								</p>
-							</li>
-						</ol>
-						<a class="button red w-50 text-center mx-auto mx-md-0" href="#payment_form">Get Yours Now!</a>
-					</div>
+
+								<ol>
+									<?php if ( have_rows( 'book_section' ) ) : ?>
+										<?php while ( have_rows('book_section' ) ) : the_row(); ?>
+											<?php if ( have_rows( 'list' ) ) : ?>
+												<?php while ( have_rows( 'list' ) ) : the_row(); ?>
+													<li>
+														<p>
+															<?php echo get_sub_field('item_text'); ?>
+														</p>
+													</li>
+												<?php endwhile; ?>
+											<?php endif; ?>
+										<?php endwhile; ?>
+									<?php endif; ?>
+								</ol>
+								<a class="button red text-center mx-auto mx-md-0" href="#payment_form">Get Yours Now!</a>
+							</div>
+
+						<?php endif; ?>
+
 					<div class="col-4 col-md-6 text-center d-flex flex-column my-auto">
 						<img class="mx-auto" src="<?php echo bloginfo( 'template_url' ); ?>/images/book-cover.png"/>
 					</div>
@@ -311,25 +323,33 @@ $paymentType = (isset($_GET['paymentType']) ? $_GET['paymentType'] : "");
 			</div>
 		</section><!-- book_section -->
 
-		<section class="daily_thoughts text-center">
-			<div class="container">
-				<div class="row">
-					<div class="col-12">
-						<h2 class="mb-4">Weekly Buddy Talk</h2>
+	<?php $weeklyTalk = get_field('weekly_talk_section');
+		if ($weeklyTalk && $weeklyTalk['video'] !== "" && $weeklyTalk['quote'] !== "") :
+	?>
+			<section class="daily_thoughts text-center">
+				<div class="container">
+					<div class="row">
+						<div class="col-12">
+							<h2 class="mb-4">Weekly Buddy Talk</h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-10 col-md-8 col-lg-6 mx-auto">
+							<!--<img src="<?php /*echo bloginfo( 'template_url' ); */?>/images/video-placeholer-success.jpg"/>-->
+							<?php echo $weeklyTalk['video'];?>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-9 col-md-6 mx-auto">
+							<h3 class="mt-5 d-inline-block">
+								<?php echo $weeklyTalk['quote'];?>
+							</h3>
+						</div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-5 mx-auto">
-						<img src="<?php echo bloginfo( 'template_url' ); ?>/images/video-placeholer-success.jpg"/>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-9 col-md-6 mx-auto">
-						<h3 class="mt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, molestiae, optio. Adipis</h3>
-					</div>
-				</div>
-			</div>
-		</section><!-- daily thoughts section -->
+			</section><!--weekly talk section -->
+
+		<?php endif; ?>
 
 </main>
 
